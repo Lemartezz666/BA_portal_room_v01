556a18c9fd1f24781cbe12802bf5e7491cfa6536
Felix <lemartesdg@gmail.com>
2020-09-16 04:36:52 +0200

    add more light and some textures

---
+++ Assets/Scripts/Flickering.cs
@@ -0,0 +1,81 @@
+﻿﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class Flickering : MonoBehaviour {
+	[Tooltip("Minimum factor multiplied with original light intensity")]
+	[Range(0, 2)]
+	public float minIntensityFactor = 0.0f;
+	[Tooltip("Maximum factor multiplied with original light intensity")]
+	[Range(0, 2)]
+	public float maxIntensityFactor = 1.4f;
+
+	[Tooltip("Pick a random intensity between min/max or switch directly between them")]
+	public bool randomIntensity = false;
+
+	[Tooltip("Interpolate between intensities or hard switches")]
+	public bool lerp = true;
+	[Tooltip("Maximum seconds it takes to interpolate between intensities")]
+	[Range(0, 1)]
+	public float maxLerpTime = 0.04f;
+
+	[Tooltip("Curve used for intensity interpolation")]
+	public AnimationCurve lerpCurve = AnimationCurve.EaseInOut(0,0,1,1);
+
+	[Tooltip("Minimum delay between flickers")]
+	[Range(0, 1)]
+	public float minDelay = 0.02f;
+	[Tooltip("Maximum delay between flickers")]
+	[Range(0, 1)]
+	public float maxDelay = 0.08f;
+
+	[Tooltip("Time the light should stay 'on'")]
+	[Range(0, 120)]
+	public float stayOnTime = 2f;
+	[Tooltip("Static 'on' intensity (0 to keep it off most of the time)")]
+	[Range(0, 2)]
+	public float stayOnIntensity = 1f;
+	[Tooltip("Probability that the light stays 'on' after a flicker")]
+	[Range(0, 1)]
+	public float stayOnProbability = 0.01f;
+
+	private float lerpTime = 0f;
+	private float lerpTimeLeft = 0f;
+	private float delayLeft = 0f;
+	private Light light;
+	private float intensity;
+	private float srcIntensity;
+	private float targetIntensity;
+	private bool turningOff = true;
+
+	void Start() {
+		light = GetComponent<Light>();
+		intensity = light.intensity;
+	}
+
+	void Update() {
+		delayLeft -= Time.deltaTime;
+		lerpTimeLeft -= Time.deltaTime;
+
+		if(delayLeft <= 0f) {
+			srcIntensity = light.intensity;
+			if(randomIntensity) {
+				targetIntensity = intensity * Random.Range(minIntensityFactor, maxIntensityFactor);
+			} else {
+				targetIntensity = intensity *(turningOff ? minIntensityFactor : maxIntensityFactor);
+				turningOff = !turningOff;
+			}
+
+			delayLeft = Random.Range(minDelay, maxDelay);
+
+			if(Random.value < stayOnProbability) {
+				delayLeft = stayOnTime;
+				targetIntensity = intensity * stayOnIntensity;
+			}
+
+			lerpTimeLeft = lerpTime = Mathf.Min(delayLeft, maxLerpTime);
+		}
+
+		light.intensity = lerp ? Mathf.Lerp(targetIntensity, srcIntensity, lerpCurve.Evaluate(lerpTime<=0.000001f ? 0 : lerpTimeLeft/lerpTime))
+			: targetIntensity;
+	}
+}
---
+++ Assets/Scripts/StaticMovement.cs
@@ -0,0 +1,38 @@
+﻿﻿using UnityEngine;
+
+public class StaticMovement : MonoBehaviour {
+	public Vector3 startOffset = new Vector3(0,0,0);
+	public Vector3 endOffset = new Vector3(0,0,0);
+
+	public float time = 1f;
+
+	public bool loop = false;
+
+	public AnimationCurve curve = AnimationCurve.EaseInOut(0,0,1,1);
+
+
+	private Vector3 lastOffset = new Vector3(0,0,0);
+
+	private float alpha = 0f;
+	private bool toEnd = true;
+
+	void Update() {
+		alpha += Time.deltaTime / time * (toEnd ? 1 : -1);
+
+		if(loop) {
+			if(toEnd && alpha>1f) {
+				alpha = 1f;
+				toEnd = false;
+			} else if(!toEnd && alpha<0f) {
+				alpha = 0f;
+				toEnd = true;
+			}
+		} else {
+			alpha = Mathf.Clamp(alpha, 0f, 1f);
+		}
+
+		var offset = Vector3.Lerp(startOffset, endOffset, curve.Evaluate(alpha));
+		transform.position += offset - lastOffset;
+		lastOffset = offset;
+	}
+}
