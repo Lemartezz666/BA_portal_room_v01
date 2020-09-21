7e3331ac5dc7c34d6d3185b83696386e8fa35796
Felix <lemartesdg@gmail.com>
2020-09-21 18:33:52 +0200

    hologramme zu den fenstern... FINAL

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,104 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+   public float Sensitivity = 0.9f;
+	
+	[Range(0,100)]
+	public float Speed = 20f;
+	[Range(0,100)]
+	public float RotationSpeed = 7f;
+	public bool LockMouse;
 
-	public float velocity = 20; // km/h
+	private float Xaxis;
+	private float Yaxis;
+	private float LastX;
+	private float LastY;
+	private float AdvanceSpeed = 1f;
+    private void LateUpdate()
+    {
+        if(Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            LockMouse = !LockMouse;
 
-	public float acceleration = 2f; // km/h²
+            if(!LockMouse)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState  = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
 
-	public float rotationSpeed = -10f;
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 
-	public float jumpForce = 500f;
-
-	public bool fixedHeight = false;
-
-	private Vector3 lastMouse;
-	private bool mouseActive = false;
-
-	private Transform rotationTransform;
-
-	private Rigidbody physicsBody;
-
-	private bool jump = false;
-
-	private Vector3 rotation;
-
-    void Start() {
-		rotationTransform = GetComponentInChildren<Camera>().transform;
-		physicsBody = GetComponent<Rigidbody>();
-
-		rotation = rotationTransform.localEulerAngles;
+        Move();
     }
 
-	void Update() {
-		if (Input.GetKeyUp(KeyCode.Space)) {
-			jump = true;
-		}
-	}
+    private void Move()
+    {
+        if(LockMouse)
+        {
+            Xaxis = Input.GetAxis("Mouse X") + LastX * Sensitivity;
+            Yaxis = -Input.GetAxis("Mouse Y") + LastY * Sensitivity;
 
-    void FixedUpdate() {
-		// rotation
-		if(Input.GetMouseButton(0)) {
-			if(mouseActive) {
-				var mouseMove = lastMouse - Input.mousePosition;
-				rotation += new Vector3(-mouseMove.y, mouseMove.x) * rotationSpeed * Time.fixedDeltaTime;
-				rotation.x = Mathf.Clamp(rotation.x, -80, 80);
-				rotationTransform.localEulerAngles = rotation;
-			}
+            LastX = Xaxis;
+            LastY = Yaxis;
 
-			mouseActive = true;
-			lastMouse = Input.mousePosition;
 
-		} else {
-			mouseActive = false;
-		}
+            transform.Rotate(RotationSpeed * Yaxis * Time.deltaTime, RotationSpeed * Xaxis * Time.deltaTime,0);
 
-		// get local movement
-		var localDir = new Vector3(0,0,0);
-        if (Input.GetKey (KeyCode.W))
-            localDir += new Vector3(0, 0 , 1);
-        if (Input.GetKey (KeyCode.S))
-            localDir += new Vector3(0, 0, -1);
-        if (Input.GetKey (KeyCode.A))
-            localDir += new Vector3(-1, 0, 0);
-        if (Input.GetKey (KeyCode.D))
-            localDir += new Vector3(1, 0, 0);
+            if(Input.GetKey(KeyCode.Space))
+            {
+                AdvanceSpeed = 5f;   
+            }
+        }
+        else
+        {
+            AdvanceSpeed = 1;
+        }
 
-		if(localDir.magnitude>1f)
-			localDir.Normalize();
-
-		// calculate global movement
-		var dir = rotationTransform.rotation * localDir;
-
-		if(physicsBody) { // first-person controller
-			var targetVelocity = dir * (velocity/3.6f);
-			var deltaVelocity = targetVelocity - physicsBody.velocity;
-			// clamp delta to acceleration
-			var deltaVelocityLen = deltaVelocity.magnitude;
-			if(deltaVelocityLen > acceleration) {
-				deltaVelocity = deltaVelocity/deltaVelocityLen * acceleration;
-			}
-
-			deltaVelocity.y = 0; // let gravity act normally
-			physicsBody.AddForce(deltaVelocity * physicsBody.mass, ForceMode.Impulse);
-
-			// jump
-			if (jump && IsGrounded()) {
-				physicsBody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-			}
-			jump = false;
-
-		} else { // freefly camera
-			if(fixedHeight)
-				dir.y = 0;
-			
-			transform.position += (dir * (velocity/3.6f) * Time.fixedDeltaTime);
-		}
+       transform.Translate(AdvanceSpeed * Speed * Input.GetAxis("Horizontal") * Time.deltaTime,0,AdvanceSpeed * Speed * Input.GetAxis("Vertical") * Time.deltaTime);
+       
+       Vector3 stayHorizontal = transform.eulerAngles;
+       stayHorizontal.z = 0f;
+       transform.eulerAngles = stayHorizontal; 
     }
-
-	private bool IsGrounded() {
-		return Physics.Raycast(transform.position + new Vector3(0, 0.2f, 0), Vector3.down, 0.5f, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore);
-	}
 }
-
